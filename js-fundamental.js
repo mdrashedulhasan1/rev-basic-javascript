ce8bd4fd0a60a67248b89e82c02831e7e62edd43
Rashedul Hasan <Rashedul Hasan>
2022-02-27 11:31:39 +0600

    JavaScript Code

---
+++ js-fundamental.js
@@ -0,0 +1,106 @@
+//Array,index,setByArray,indexof
+// var ages = [12, 22, 34, 14, 56, 27];
+// ages[2] = 35;
+// console.log(ages);
+
+// var sarowal = ages[1];
+// console.log(sarowal);
+
+// var position = ages.indexOf(56);
+// console.log(position);
+
+
+//push,pop,unshift,shift
+// var friendList = ["sarowal", "rakib"];
+// friendList.push("hasan");
+// friendList.pop();
+// friendList.unshift("tapu");
+// friendList.shift();
+// console.log(friendList);
+
+
+//while loop
+// var i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+//for loop
+// for(var i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// var friendsName = ["sarowal", "rakib", "mehadi"];
+// for(var i=0; i<friendsName.length; i++){
+//     var friendName = friendsName[i];
+//     console.log(friendName);
+// }
+
+
+//switch case
+// var number = 2;
+// switch(number){
+//     case 1:
+//         console.log("One");
+//         break;
+//     case 2:
+//         console.log("Two");
+//         break;
+//     case 3:
+//         console.log("Three");
+//         break;
+//     case 4:
+//         console.log("Four");
+//         break;
+//     case 5:
+//         console.log("Five");
+//         break;
+//     default:
+//         console.log("Number not found!");
+// }
+
+
+
+//Function
+// function address(name, city){
+//     console.log("Name:",name);
+//     console.log("City:",city);
+// }
+// address("Tapu","Tangail");
+
+//Addition Function
+// function add(a,b){
+//     result = a+b;
+//     return result;
+// }
+// var total = add(2,3);
+// console.log(total);
+
+//doubleIt Function
+// function doubleIt(num){
+//     var double = num*2;
+//     return double;
+// }
+// var firstNumber = doubleIt(5);
+// var secondNumber = doubleIt(3);
+// var thirdNumber = doubleIt(10);
+// var total = firstNumber+secondNumber+thirdNumber;
+// console.log(total);
+
+
+//Object
+// var personInfo = {
+//     id:"01",
+//     name1:"Tapu",
+//     phone:"017*********",
+//     city:"Tangail"
+// }
+
+// personInfo.name1 = "Rashedul";
+// personInfo["phone"] = "0171******";
+
+// console.log(personInfo.id);
+// console.log(personInfo["name1"]);
+// console.log(personInfo.phone);
+// console.log(personInfo.city);
---
+++ js-start.js
@@ -0,0 +1,106 @@
+// Variable declare, see output
+// var mangoPrice = 10;
+// console.log(mangoPrice);
+
+//Variable type
+// var mangoPrice = 20;
+// console.log(typeof mangoPrice);
+
+// var myName = "Rashedul Hasan";
+// console.log(typeof myName);
+
+// var isGood = true;
+// console.log(typeof isGood);
+// var isBad = false;
+// console.log(typeof isBad);
+
+// Explore String
+// var myCountry = "I love Bangladesh";
+// var update = myCountry.toLowerCase(myCountry);
+// console.log(update);
+// var update2 = myCountry.toUpperCase(myCountry);
+// console.log(update2);
+// var index = myCountry.indexOf("love");
+// console.log(index);
+// var split = myCountry.split(" ");
+// console.log(split);
+
+//parseInt, parseFloat
+// var num = 5;
+// var num2 = parseFloat('3.36');
+// var result = num+num2;
+// console.log(result);
+
+//Mathematical Operation
+// var num = 10;
+// var num2 = 3;
+// var add = num+num2;
+// var sub = num-num2;
+// var multi = num*num2;
+// var div = num/num2;
+// var div = div.toFixed(2);
+// var rem = num%num2;
+// console.log("Add:",add,"Sub:",sub,"Multi:",multi,"Div:",div,"Rem:",rem);
+
+//String Addition
+// var firstName = "Rashedul";
+// var lastName = "Hasan";
+// var fullName = firstName+" "+lastName;
+// console.log(fullName);
+
+//Math abs(),floor(),ceil(),round(),random()
+// var num = Math.abs(-5465);
+// console.log(num);
+
+// var num2 = Math.floor(23.455);
+// console.log(num2);
+
+// var num3 = Math.ceil(23.455);
+// console.log(num3);
+
+// var num4 = Math.round(23.4999);
+// console.log(num4);
+
+// var num5 = Math.round(23.5);
+// console.log(num5);
+
+// var num6 = Math.random(); //[0 to 1]
+// console.log(num6);
+
+// var num7 = Math.random()*100; //[0 to 100]
+// var num7 = Math.round(num7);
+// console.log(num7);
+
+
+//Increment, Decrement
+// var num = 5;
+// num++; //6
+// num++; //7
+// num--; //6
+// num++; //7
+// num--; //6
+// num++; //7
+// console.log(num);
+
+//if,else,else if condition
+//Using >,>=,<,<=,==,!=
+// var mangoPrice = 20;
+// if(mangoPrice != 20){  
+//     console.log("You can buy mango");
+// }
+// else{
+//     console.log("You can't buy mango")
+// }
+
+//Using &&,||
+// var friend1Take = 100;
+// var friend2Take = 70;
+// if(friend1Take == 100 && friend2Take == 50){  
+//     console.log("Go to restaurant-01");
+// }
+// else if(friend1Take>=100 || friend2Take>=100){
+//     console.log("Go to restaurant-02");
+// }
+// else{
+//     console.log("Can't go to restaurant");
+// }
